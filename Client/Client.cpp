2e57ff2b0dc2d8b76323af76e68a0cdf78299c47
MiFeng-super <73420730+MiFeng-super@users.noreply.github.com>
2022-11-13 21:25:48 +0800

    添加项目文件。

---
+++ Client/Client.cpp
@@ -0,0 +1,80 @@
+﻿// Client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "..\include\Veil\Veil.h"
+
+constexpr const wchar_t* LPCPortName = L"\\LPCTest";
+constexpr const size_t   MessageLen = 100;
+
+
+LPVOID allocMessage(size_t msgsize)
+{
+	return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(PORT_MESSAGE) + msgsize);
+}
+
+
+int main()
+{
+	SECURITY_QUALITY_OF_SERVICE sqos { 0 };
+
+	UNICODE_STRING portName			{ 0 };
+	NTSTATUS	   status			= 0;
+	HANDLE		   hPort			= nullptr;
+	ULONG		   MaxMessageLength = 0;
+	LPVOID		   lpMem			= nullptr;
+	LPVOID		   lpMem2			= nullptr;
+
+	RtlInitUnicodeString(&portName, LPCPortName);
+
+	status = NtConnectPort(&hPort, &portName, &sqos, nullptr, nullptr, &MaxMessageLength, nullptr, nullptr);
+
+	std::cout << "[i] NtConnectPort: 0x" << std::hex << status << std::endl;
+
+	if (NT_SUCCESS(status))
+	{
+		auto messageLength = MaxMessageLength - sizeof(PORT_MESSAGE);
+
+		lpMem  = allocMessage(messageLength);
+		lpMem2 = allocMessage(messageLength);
+
+		if (lpMem)
+		{
+			auto* reqMsg  = (PORT_MESSAGE*)lpMem;
+			auto* reqBuf   = (char*)lpMem + sizeof(PORT_MESSAGE);
+
+			auto* replyMsg = (PORT_MESSAGE*)lpMem2;
+			auto* replyBuf = (char*)lpMem2 + sizeof(PORT_MESSAGE);
+
+
+			while (true)
+			{
+				std::cout << "[.] Request Message > ";
+
+				fgets(reqBuf, messageLength, stdin);
+
+				reqMsg->u1.s1.DataLength  = strlen(reqBuf) + 1;
+				reqMsg->u1.s1.TotalLength = reqMsg->u1.s1.DataLength + sizeof(PORT_MESSAGE);
+
+				replyMsg->u1.s1.DataLength  = messageLength;
+				replyMsg->u1.s1.TotalLength = replyMsg->u1.s1.DataLength + sizeof(PORT_MESSAGE);
+
+				status = NtRequestWaitReplyPort(hPort, reqMsg, replyMsg);
+
+				if (!NT_SUCCESS(status))
+				{
+					break;
+				}
+
+				std::cout << "[i] Reply Message > " << replyBuf << std::endl;
+
+			}
+
+			HeapFree(GetProcessHeap(), 0, lpMem);
+			HeapFree(GetProcessHeap(), 0, lpMem2);
+		}
+
+		NtClose(hPort);
+	}
+}
+
---
+++ Server/Server.cpp
@@ -0,0 +1,115 @@
+﻿// Server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <thread>
+#include "..\include\Veil\Veil.h"
+
+constexpr const wchar_t* LPCPortName = L"\\LPCTest";
+constexpr const size_t   MessageLen  = 100;
+
+
+LPVOID allocMessage(size_t msgsize) 
+{
+    return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(PORT_MESSAGE) + msgsize);
+}
+
+
+void serverThread() 
+{
+    UNICODE_STRING      portName { 0 };
+    OBJECT_ATTRIBUTES   objAttr  { 0 };
+    NTSTATUS            status  = 0;
+    HANDLE              hPort   = nullptr;
+    HANDLE              hClient = nullptr;
+    LPVOID              lpMem   = nullptr;
+
+    RtlInitUnicodeString(&portName, LPCPortName);
+    InitializeObjectAttributes(&objAttr, &portName, 0, nullptr, nullptr);
+
+    status = NtCreatePort(&hPort, &objAttr, 0, sizeof(PORT_MESSAGE) + MessageLen, 0);
+
+	std::cout << "[i] NtCreatePort: 0x" << std::hex << status << std::endl;
+
+    if (NT_SUCCESS(status))
+    {
+        lpMem = allocMessage(MessageLen);
+        if (lpMem)
+        {
+            auto* message = (PORT_MESSAGE*)lpMem;
+            auto* buffer  = (char*)lpMem + sizeof(PORT_MESSAGE);
+
+            while (true)
+            {
+				status = NtReplyWaitReceivePort(hPort, nullptr, nullptr, message);
+                if (!NT_SUCCESS(status))
+                {
+                    break;
+                }
+
+                switch (message->u2.s2.Type)
+                {
+				    case LPC_CONNECTION_REQUEST:
+
+                        std::cout << "[i] NtReplyWaitReceivePort: LPC_CONNECTION_REQUEST" << std::endl;
+
+                        if (hClient == nullptr)
+                        {
+							status = NtAcceptConnectPort(&hClient, nullptr, message, true, nullptr, nullptr);
+							if (NT_SUCCESS(status))
+							{
+								status = NtCompleteConnectPort(hClient);
+
+								if (!NT_SUCCESS(status))
+								{
+                                    NtClose(hClient);
+                                    hClient = nullptr;
+								}
+							}
+                        }
+
+					    break;
+
+
+                    case LPC_REQUEST:
+
+                        std::cout << "[i] NtReplyWaitReceivePort: LPC_REQUEST" << std::endl;
+                        std::cout << "[i] Received Data: " << buffer << std::endl;
+
+                        NtReplyPort(hPort, message);
+
+                        break;
+
+                    case LPC_PORT_CLOSED:
+                    case LPC_CLIENT_DIED:
+
+						std::cout << "[i] NtReplyWaitReceivePort: LPC_PORT_CLOSED or LPC_CLIENT_DIED" << std::endl;
+                        
+                        if (hClient)
+                        {
+                            NtClose(hClient);
+                            hClient = nullptr;
+                        }
+
+                        break;
+                }
+            }
+
+
+            HeapFree(GetProcessHeap(), 0, lpMem);
+        }
+
+        NtClose(hPort);
+    }
+}
+
+
+int main()
+{
+    std::thread t (serverThread);
+
+    t.join();
+
+    return 0;
+}
+
